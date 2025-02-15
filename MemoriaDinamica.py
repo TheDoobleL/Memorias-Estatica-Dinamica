f6eed56b63ea02f26b6b8c93c4d773e9fa1b5daa
Apolo Eleven <luiscarrillo152@gmail.com>
2025-02-14 18:39:46 -0600

    Add files via upload

---
+++ MemoriaDinamica.py
@@ -0,0 +1,15 @@
+def main():
+    
+    frutas = []
+    frutas.append("Mango")
+    frutas.append("Manzana")
+    frutas.append("Platano")
+    frutas.append("Uva")
+    print(frutas)
+    frutas.pop(0)
+    frutas.pop(1)
+    frutas.append("Sandia")
+    print(frutas)
+
+if __name__ == "__main__":
+    main()
---
+++ MemoriaEstatica.py
@@ -0,0 +1,7 @@
+calificaciones = []
+
+for i in range(5):
+    calificacion = int(input("Capture una Calificación: "))
+    calificaciones.append(calificacion)
+
+print("Las calificaciones guardadas son:", calificaciones)
